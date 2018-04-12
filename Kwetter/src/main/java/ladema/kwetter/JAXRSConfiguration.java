521e00e7c41832260541b0dfc774159032e5df85
LaurensAdema <laurensadema@live.nl>
2018-04-12 16:24:58 +0200

    Delete old project

--- Kwetter/src/main/java/ladema/kwetter/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ladema.kwetter;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class JAXRSConfiguration extends Application {
-
-}
