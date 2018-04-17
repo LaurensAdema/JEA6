b8929e0150fe10f22b0a4d0e46cd480abef0c251
LaurensAdema <laurensadema@live.nl>
2018-04-17 16:40:06 +0200

    Removed arquillian

--- src/test/java/ma/ade/kwetter2/test/service/TweetServiceTest.java
+++ src/test/java/ma/ade/kwetter2/test/service/TweetServiceTest.java
@@ -1,27 +1,10 @@
 package ma.ade.kwetter2.test.service;
 
-import ma.ade.kwetter2.service.TweetService;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-@RunWith(Arquillian.class)
 public class TweetServiceTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(TweetService.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-
     @Before
     public void setUp() throws Exception {
     }
--- src/test/java/ma/ade/kwetter2/test/service/UserServiceTest.java
+++ src/test/java/ma/ade/kwetter2/test/service/UserServiceTest.java
@@ -1,26 +1,10 @@
 package ma.ade.kwetter2.test.service;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
-@RunWith(Arquillian.class)
 public class UserServiceTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(ma.ade.kwetter2.service.UserService.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-
     @Before
     public void setUp() throws Exception {
     }
